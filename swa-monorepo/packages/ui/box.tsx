0d48f91611651569ec978bf38538e5c932e79f48
weilderbeast <bogdan.is.a.beast@gmail.com>
2022-09-22 21:07:00 +0200

    add custom component to UI package

---
+++ swa-monorepo/packages/ui/box.tsx
@@ -0,0 +1,13 @@
+type BoxProps = React.HTMLProps<HTMLDivElement> & {
+  children: React.ReactNode;
+};
+/*
+    Style inspired by Chakra-UI, where css is passed as props,
+    making it a lot easier to use and share across components.
+
+    Also helps scope css without worrying about overlap.
+    Also provides type safety.
+*/
+export const Box = ({ children, ...props }: BoxProps) => {
+  return <div style={{ ...props }}>{children}</div>;
+};
